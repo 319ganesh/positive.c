2271fb94aa41f093fc7e46f8a64f41808e671d3a
319ganesh <31438442+319ganesh@users.noreply.github.com>
2018-02-02 16:04:38 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
+
 main()
 {
 char c;
-clrscr();
+
 printf("\n enter a letter:");
 scanf("%c",&c);
 if((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
@@ -14,6 +14,5 @@ else
 {
 printf("\n it is not an alphabet");
 }
-getch();
-return 0;
+  return 0;
 }
